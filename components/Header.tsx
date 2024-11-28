2b60a453708b27b07d9f2f21b3fe36cb3e2a675d
Matan <matan3sh@gmail.com>
2024-11-28 19:03:40 +0200

    Start Implementing Header Component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,87 @@
 'use client'
 
-import { useUser } from '@clerk/nextjs'
+import {
+  ClerkLoaded,
+  SignedIn,
+  SignInButton,
+  UserButton,
+  useUser,
+} from '@clerk/nextjs'
+import { PackageIcon, TrolleyIcon } from '@sanity/icons'
+import Form from 'next/form'
 import Link from 'next/link'
 
 const Header = () => {
   const { user } = useUser()
 
+  const createClerkPasskey = async () => {}
+
   console.log(user)
 
   return (
     <header className="flex flex-wrap justify-between items-center px-4 py-2">
-      <div>
+      <div className="flex w-full flex-wrap justify-between items-center">
         <Link
           className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
           href="/"
         >
           Shopr
         </Link>
+
+        <Form
+          className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
+          action="/search"
+        >
+          <input
+            className="bg-gray-100 text-gray-800 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border w-full max-w-4xl"
+            name="query"
+            type="text"
+            placeholder="Search products"
+          />
+        </Form>
+
+        <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none">
+          <Link
+            className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            href="/basket"
+          >
+            <TrolleyIcon className="w-6 h-6" />
+            <span>My Basket</span>
+          </Link>
+
+          <ClerkLoaded>
+            <SignedIn>
+              <Link
+                href="/orders"
+                className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                <PackageIcon className="w-6 h-6" />
+                <span>My Orders</span>
+              </Link>
+            </SignedIn>
+
+            {user ? (
+              <div className="flex items-center space-x-2">
+                <UserButton />
+                <div className="hidden sm:block text-xs">
+                  <p className="text-gray-400">Welcome Back</p>
+                  <p className="font-bold">{user.fullName}</p>
+                </div>
+              </div>
+            ) : (
+              <SignInButton mode="modal" />
+            )}
+
+            {user?.passkeys.length === 0 && (
+              <button
+                onClick={createClerkPasskey}
+                className="bg-white hover:bg-blue-700 hover:text-white animate-pulse text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
+              >
+                Create a passkey
+              </button>
+            )}
+          </ClerkLoaded>
+        </div>
       </div>
     </header>
   )
